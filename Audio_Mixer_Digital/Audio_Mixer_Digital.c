39366dc04adf08c98845daff0bbd3c0fd01629bb
andres-caceres <acaceresg@pucp.edu.pe>
2022-01-28 11:59:40 -0500

    primer commit

---
+++ Audio_Mixer_Digital/Audio_Mixer_Digital.c
@@ -0,0 +1,95 @@
+/*******************************
+  Audio Mixer Digital Firmware
+  Based on deej by Omri Harel
+  version: pre-alpha 1.0
+*******************************/
+
+#define DEBUG 0
+
+const int NUM_ENCODERS = 5;
+const int clkInputs[NUM_ENCODERS] = {A0, A1, A2, A3, A4};
+const int dtInputs[NUM_ENCODERS] = {A0, A1, A2, A3, A4};
+const int swInputs[NUM_ENCODERS] = {A0, A1, A2, A3, A4};
+
+int values[NUM_ENCODERS];
+
+void setup() { 
+  for (int i = 0; i < NUM_ENCODERS; i++) {
+    
+    pinMode(clkInputs[i], INPUT);
+    pinMode(dtInputs[i], INPUT);
+    pinMode(swInputs[i], INPUT);
+    
+  }
+
+  Serial.begin(9600);
+
+  initWelcome();
+}
+
+void loop() {
+  
+  updateValues();
+  sendValues(); // Actually send data (all the time)
+  //printValues(); // For debug
+  
+  delay(10);
+  //delay(1000); // For debug
+}
+
+void updateValues() {
+
+  for (int i = 0; i < NUM_ENCODERS; i++) {
+
+     currentStateCLK = digitalRead(clkInputs);
+     
+     if (currentStateCLK != previousStateCLK){
+        
+        if (digitalRead(inputDT) != currentStateCLK) {
+            // Encoder is rotating counterclockwise
+            value --;
+        }
+        else {
+           // Encoder is rotating clockwise
+            value ++;
+        }
+
+        if value>1023 {value=1023};
+        if value<0    {value=0};
+
+        values[i]=value;
+     }
+
+     //update previous state
+     previousStateCLK = currentStateCLK;
+
+  }
+}
+
+void sendValues() {
+  String builtString = String("");
+
+  for (int i = 0; i < NUM_ENCODERS; i++) {
+    builtString += String((int)values[i]);
+
+    if (i < NUM_ENCODERS - 1) {
+      builtString += String("|");
+    }
+  }
+  
+  Serial.println(builtString);
+}
+
+
+void printValues() {
+  for (int i = 0; i < NUM_ENCODERS; i++) {
+    String printedString = String("Encoder #") + String(i + 1) + String(": ") + String(Values[i]) + String("u");
+    Serial.write(printedString.c_str());
+
+    if (i < NUM_ENCODERS - 1) {
+      Serial.write(" | ");
+    } else {
+      Serial.write("\n");
+    }
+  }
+}
